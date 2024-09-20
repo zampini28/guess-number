5290e25732ce799dde6e3efbe85859c5ed3e2da4
zampini28 <caio.zampini@gmail.com>
2024-09-20 20:14:45 -0300

    minor fixes

--- train_model.py
+++ train_model.py
@@ -3,24 +3,24 @@ import numpy as np
 from tensorflow import keras
 from tensorflow.keras import layers
 
-def load_training_data():
+def load_training_data(dataset_dir='dataset'):
     dataset, labels = zip(*[
         (data['grid'], data['label'])
-        for filename in os.listdir('dataset') if filename.endswith('.npy')
-        for data in [np.load(os.path.join('dataset', filename), allow_pickle=True).item()]
+        for filename in os.listdir(dataset_dir) if filename.endswith('.npy')
+        for data in [np.load(os.path.join(dataset_dir, filename), allow_pickle=True).item()]
     ])
 
     return np.array(dataset), np.array(labels)
 
 def preprocess_data(x):
-    return (x.astype('float32') / 1.0).reshape(x.shape[0], -1)
+    return x.astype('float32').reshape(x.shape[0], -1)
 
 def create_model(input_shape):
     model = keras.Sequential([
         layers.Input(shape=input_shape),
         layers.Dense(128, 'relu'),
         layers.Dense(64, 'relu'),
-        layers.Dense(10, 'softmax'),
+        layers.Dense(9, 'softmax'),
     ])
 
     model.compile('adam', 'sparse_categorical_crossentropy', None, ['accuracy'])
